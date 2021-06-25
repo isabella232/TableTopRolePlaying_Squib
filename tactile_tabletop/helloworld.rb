e40e1100e9510e175d4a4b2c192fce2e3aa2ac30
Jason Nelson <jason.andr.nelson@gmail.com>
2021-06-24 21:00:23 -0700

    updating rb file for new csv naming convention

--- tactile_tabletop/helloworld.rb
+++ tactile_tabletop/helloworld.rb
@@ -2,15 +2,15 @@ require 'squib'
 
 data = Squib.csv file: 'testCardInput.csv'
 
-Squib::Deck.new cards: data['topAbilityName'].size, layout: 'layout.yml' do
+Squib::Deck.new cards: data['Top Ability Name'].size, layout: 'layout.yml' do
   background color: 'white'
   rect layout: 'safe'
   rect layout: 'cut'
-  text str: data['topAbilityName'], layout: 'title1'
-  text str: data['topAbility'], layout: 'desc1'
+  text str: data['Top Ability Name'], layout: 'title1'
+  text str: data['Top Ability Rules'], layout: 'desc1'
   
-  text str: data['bottomAbilityName'], layout: 'title2'
-  text str: data['bottomAbility'], layout: 'desc2'
+  text str: data['Bottom Ability Name'], layout: 'title2'
+  text str: data['Bottom Ability Rules'], layout: 'desc2'
   
   save_png prefix: 'ttcc_'
   save_pdf trim: 37.5
