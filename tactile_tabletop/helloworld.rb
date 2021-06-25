fe266f148e5c5766a252eb65046ba809fc91983f
Jason Nelson <jason.andr.nelson@gmail.com>
2021-06-24 21:03:24 -0700

    fixing some var names

--- tactile_tabletop/helloworld.rb
+++ tactile_tabletop/helloworld.rb
@@ -6,11 +6,11 @@ Squib::Deck.new cards: data['Top Ability Name'].size, layout: 'layout.yml' do
   background color: 'white'
   rect layout: 'safe'
   rect layout: 'cut'
-  text str: data['Top Ability Name'], layout: 'title1'
-  text str: data['Top Ability Rules'], layout: 'desc1'
+  text str: data['Top Ability Name'], layout: 'topTitle'
+  text str: data['Top Ability Rules'], layout: 'topRules'
   
-  text str: data['Bottom Ability Name'], layout: 'title2'
-  text str: data['Bottom Ability Rules'], layout: 'desc2'
+  text str: data['Bottom Ability Name'], layout: 'bottomTitle'
+  text str: data['Bottom Ability Rules'], layout: 'bottomRules'
   
   save_png prefix: 'ttcc_'
   save_pdf trim: 37.5
