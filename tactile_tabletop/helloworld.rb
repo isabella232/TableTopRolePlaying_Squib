4f827d586b3b50e5b08e860c35809028900373f7
Jason Nelson <jason.andr.nelson@gmail.com>
2021-06-25 20:24:52 -0700

    reorganizing and renaming variables for the after-ability-card-action stuff

--- tactile_tabletop/helloworld.rb
+++ tactile_tabletop/helloworld.rb
@@ -1,16 +1,20 @@
 require 'squib'
 
-data = Squib.csv file: 'testCardInput.csv'
+data = Squib.csv file: 'Action Cards - CharacterCards.csv'
 
 Squib::Deck.new cards: data['Top Ability Name'].size, layout: 'layout.yml' do
   background color: 'white'
   rect layout: 'safe'
   rect layout: 'cut'
+  line layout: 'line'
+  
   text str: data['Top Ability Name'], layout: 'topTitle'
   text str: data['Top Ability Rules'], layout: 'topRules'
+  png layout: data['discardTop']
   
   text str: data['Bottom Ability Name'], layout: 'bottomTitle'
   text str: data['Bottom Ability Rules'], layout: 'bottomRules'
+  png layout: data['discardBottom']
   
   save_png prefix: 'ttcc_'
   save_pdf trim: 37.5
