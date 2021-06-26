a66265df47289f00e4ea562d8d1c661cd4cf1977
Jason Nelson <jason.andr.nelson@gmail.com>
2021-06-25 21:41:20 -0700

    adding passives and requirements, adding lines about passive and requirements, adding top target (still needs work), adding references to csv contents for passive, requirements, and top target

--- tactile_tabletop/helloworld.rb
+++ tactile_tabletop/helloworld.rb
@@ -6,16 +6,26 @@ Squib::Deck.new cards: data['Top Ability Name'].size, layout: 'layout.yml' do
   background color: 'white'
   rect layout: 'safe'
   rect layout: 'cut'
-  line layout: 'line'
   
   text str: data['Top Ability Name'], layout: 'topTitle'
+  rect layout: 'topTargetBox'
+  text str: data['Top Ability Target'], layout: 'topTarget'
   text str: data['Top Ability Rules'], layout: 'topRules'
   png layout: data['discardTop']
   
+  rect layout: 'lineTopOfBottomAbility'
   text str: data['Bottom Ability Name'], layout: 'bottomTitle'
   text str: data['Bottom Ability Rules'], layout: 'bottomRules'
   png layout: data['discardBottom']
   
+  rect layout: 'lineTopOfPassives'
+  text str: "Passives", layout: 'passivesTitle'
+  text str: data['Passives'], layout: 'passivesBody'
+  
+  rect layout: 'lineTopOfRequirements'
+  text str: "Requirements", layout: 'requirementsTitle'
+  text str: data['Requirements'], layout: 'requirementsBody'
+  
   save_png prefix: 'ttcc_'
-  save_pdf trim: 37.5
+  #save_pdf trim: 37.5
 end
