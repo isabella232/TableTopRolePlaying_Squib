0f3fc83186ae61d82269729edccf23c281bbaba4
Jason Nelson <jason.andr.nelson@gmail.com>
2021-06-26 21:16:12 -0700

    messing more with what's commented out/what has rect around them for measuring

--- tactile_tabletop/helloworld.rb
+++ tactile_tabletop/helloworld.rb
@@ -12,27 +12,26 @@ Squib::Deck.new(dpi: 300, width: 816, height: 1110, cards: data['Top Ability Nam
   rect layout: 'cut'
   
   text str: data['Top Ability Name'], layout: 'topTitle'
-  rect layout: 'topTargetBox'
+  #rect layout: 'topTargetBox'
   #text str: data['Top Ability Target'], layout: 'topTarget'
-  #text str: data['Top Ability Rules'], layout: 'topRules'
+  text str: data['Top Ability Rules'], layout: 'topRules'
 
   #svg layout: data['discardTop']
-  
-  rect layout: 'topTitle'
-  #rect layout: 'topRules'
   
   rect layout: 'lineTopOfBottomAbility'
   text str: data['Bottom Ability Name'], layout: 'bottomTitle'
   text str: data['Bottom Ability Rules'], layout: 'bottomRules'
-  png layout: data['discardBottom']
+  #png layout: data['discardBottom']
   
   rect layout: 'lineTopOfPassives'
+  rect layout: 'passivesTitle'
+  rect layout: 'passivesBody'
   text str: "Passives", layout: 'passivesTitle'
   text str: data['Passives'], layout: 'passivesBody'
   
   rect layout: 'lineTopOfRequirements'
-  text str: "Requirements", layout: 'requirementsTitle'
-  text str: data['Requirements'], layout: 'requirementsBody'
+  #text str: "Requirements", layout: 'requirementsTitle'
+  #text str: data['Requirements'], layout: 'requirementsBody'
   
   save_png prefix: 'ttcc_'
   #save_pdf trim: 37.5
