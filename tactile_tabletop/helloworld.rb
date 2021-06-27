759a174b1eeb5ed5c96782a8d66808e40987fe88
Jason Nelson <jason.andr.nelson@gmail.com>
2021-06-26 23:06:02 -0700

    commenting out rects, fixing references to icons

--- tactile_tabletop/helloworld.rb
+++ tactile_tabletop/helloworld.rb
@@ -2,6 +2,9 @@ require 'squib'
 require 'game_icons'
 
 data = Squib.csv file: 'Action Cards - CharacterCards.csv'
+#grabbing icons from https://game-icons.net/
+#using gem game_icons to be able to load them
+
 
 #width/height/dpi measurements provided by template from BoardGameMaker.com, see American-poker-size.pdf
 Squib::Deck.new(dpi: 300, width: 816, height: 1110, cards: data['Top Ability Name'].size, layout: 'layout.yml')  do
@@ -13,25 +16,25 @@ Squib::Deck.new(dpi: 300, width: 816, height: 1110, cards: data['Top Ability Nam
   rect layout: 'cut'
   
   ## top ability stuff
-  rect layout: 'lineRightOfBubbles'
+  #rect layout: 'lineRightOfBubbles'
   text str: data['Top Ability Target'], layout: 'topTarget'
   rect layout: 'topTargetBox'
   
-  rect layout: 'topTitle'
-  rect layout: 'topRules'
+  #rect layout: 'topTitle'
+  #rect layout: 'topRules'
   text str: data['Top Ability Name'], layout: 'topTitle'
   text str: data['Top Ability Rules'], layout: 'topRules'
-  svg layout: data['discardTop']
-  
+  svg file: data['Top Ability Following Card Action'].map {|t| "to_#{t.downcase}.svg" }, layout: 'topAfterAbilityLocation'
+
   ## bottom ability stuff
   
   rect layout: 'lineTopOfBottomAbility'
-  rect layout: 'bottomTitle'
-  rect layout: 'bottomRules'
+  #rect layout: 'bottomTitle'
+  #rect layout: 'bottomRules'
   text str: data['Bottom Ability Name'], layout: 'bottomTitle'
   text str: data['Bottom Ability Rules'], layout: 'bottomRules'
-  svg layout: data['discardBottom']
-  
+  svg file: data['Bottom Ability Following Card Action'].map {|t| "to_#{t.downcase}.svg" }, layout: 'bottomAfterAbilityLocation'
+
   ## passives stuff
   
   rect layout: 'lineTopOfPassives'
