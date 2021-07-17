d520d17e6980d0635677c9dffa2d9b5ddad44076
Jason Nelson <jason.andr.nelson@gmail.com>
2021-07-17 09:01:27 -0700

    mapping die inputs into svg files, adding text from csv into card

--- tactile_tabletop/generate-weapons.rb
+++ tactile_tabletop/generate-weapons.rb
@@ -19,20 +19,21 @@ Squib::Deck.new(dpi: 300, width: 820, height: 820, cards: data['Weapon Name'].si
   text str: data['Weapon Name'], layout: 'WeaponTitle'
   
   text str: "Attack Die:", layout: 'AttackTitle'
-  svg data: GameIcons.get(data['Attack Die 1']).recolor(fg: '777', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'AttackDie'
-  svg data: GameIcons.get(data['Attack Die 2']).recolor(fg: '777', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'AttackDie'
-
+  svg file: data['Attack Die 1'].map {|t| "#{t.downcase}.svg" }, layout: 'AttackDie1'  
+  svg file: data['Attack Die 2'].map {|t| "#{t.downcase}.svg" }, layout: 'AttackDie2'
+  
   text str: "Defense Die:", layout: 'DefenseTitle'
-  #
+  svg file: data['Defense Die'].map {|t| "#{t.downcase}.svg" }, layout: 'DefenseDie'
   
   text str: "Range:", layout: 'RangeTitle'
-  #
+  text str: data['Range'], layout: 'RangeValue'
   
   text str: "Requirements:", layout: 'RequirementsTitle'
-  #
+  svg file: data['Requirement'].map {|t| "#{t.downcase}.svg" }, layout: 'RequirementsValue'
   
   text str: "Notes:", layout: 'NotesTitle'
-  #
+  rect layout: 'NotesTitle'
+  #text str: data['Extra Notes'], layout: 'NotesValue'
   
   # rect layout: 'topDurationBubble'
   # text str: data['Top Ability Duration'], layout: 'topDuration'
