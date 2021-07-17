427892ed3e01a17553d3245c06b7e5ea4e2d38b4
Jason Nelson <jason.andr.nelson@gmail.com>
2021-07-17 09:13:02 -0700

    fix opacity on icon, removing commented stuff

--- tactile_tabletop/generate-weapons.rb
+++ tactile_tabletop/generate-weapons.rb
@@ -32,56 +32,7 @@ Squib::Deck.new(dpi: 300, width: 820, height: 820, cards: data['Weapon Name'].si
   svg file: data['Requirement'].map {|t| "#{t.downcase}.svg" }, layout: 'RequirementsValue'
   
   text str: "Notes:", layout: 'NotesTitle'
-  rect layout: 'NotesTitle'
   text str: data['Extra Notes'], layout: 'NotesValue'
-  rect layout: 'NotesValue'
-  
-  # rect layout: 'topDurationBubble'
-  # text str: data['Top Ability Duration'], layout: 'topDuration'
-  # svg data: GameIcons.get('stopwatch').recolor(fg: '777', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'topDurationIcon'
-
-  
-  # #rect layout: 'topTitle'
-  # #rect layout: 'topVariables'
-  # #rect layout: 'topRules'
-  # text str: data['Top Ability Name'], layout: 'topTitle'
-  # text str: data['Top Ability Die Roll/Scaler'], layout: 'topVariables'
-  # text str: data['Top Ability Rules'], layout: 'topRules'
-  # svg file: data['Top Ability Following Card Action'].map {|t| "to_#{t.downcase}.svg" }, layout: 'topAfterAbilityLocation'
-
-  # ## bottom ability stuff
-  
-  # rect layout: 'lineTopOfBottomAbility'
-  # rect layout: 'bottomTargetBubble'
-  # text str: data['Bottom Ability Target'], layout: 'bottomTarget'
-  # rect layout: 'bottomDurationBubble'
-  # text str: data['Bottom Ability Duration'], layout: 'bottomDuration'  
-  # #rect layout: 'bottomTitle'
-  # #rect layout: 'bottomRules'
-  # #rect layout: 'bottomVariables'
-  # text str: data['Bottom Ability Name'], layout: 'bottomTitle'
-  # text str: data['Bottom Ability Die Roll/Scaler'], layout: 'bottomVariables'
-  # svg data: GameIcons.get('crosshair').recolor(fg: '777', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'bottomTargetIcon'
-  
-  # text str: data['Bottom Ability Rules'], layout: 'bottomRules'
-  # svg file: data['Bottom Ability Following Card Action'].map {|t| "to_#{t.downcase}.svg" }, layout: 'bottomAfterAbilityLocation'
-  # svg data: GameIcons.get('stopwatch').recolor(fg: '777', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'bottomDurationIcon'
-
-  # ## passives stuff
-  
-  # rect layout: 'lineTopOfPassives'
-  # #rect layout: 'passivesTitle'
-  # #rect layout: 'passivesBody'
-  # text str: "Passives", layout: 'passivesTitle'
-  # text str: data['Passives'], layout: 'passivesBody'
-  
-  # ## requirements stuff
-  
-  # rect layout: 'lineTopOfRequirements'
-  # #rect layout: 'requirementsTitle'
-  # #rect layout: 'requirementsBody'
-  # text str: "Requirements", layout: 'requirementsTitle'
-  # text str: data['Requirements'], layout: 'requirementsBody'
 
   # ## output file stuff
 
