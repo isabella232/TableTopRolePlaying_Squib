28f561726686e11c11e56799a041d53129ee740f
Jason Nelson <jason.andr.nelson@gmail.com>
2022-01-27 21:30:10 -0800

    adding a section to generate armor cards with armor csv from google docs

---
+++ tactile_tabletop/Armor Cards/generate-armors.rb
@@ -0,0 +1,42 @@
+require 'squib'
+require 'game_icons'
+
+data = Squib.csv file: 'Tactile Tabletop Data - Armor.csv'
+#grabbing icons from https://game-icons.net/
+#using gem game_icons to be able to load them
+
+
+#width/height/dpi measurements provided by template from BoardGameMaker.com, see poker-size.pdf
+Squib::Deck.new(dpi: 300, width: 820, height: 820, cards: data['Armor Name'].size, layout: 'armorcardlayout.yml')  do
+
+  ## overall card stuff
+
+  background color: 'black'
+  rect layout: 'cut'
+  rect layout: 'safe'
+  
+  ## top ability stuff
+  #rect layout: 'ArmorTitle'
+  text str: data['Armor Name'], layout: 'ArmorTitle'
+  #rect layout: 'ArmorAbility'
+  text str: data['Set Ability 1'], layout: 'ArmorAbility'
+
+  # ## output file stuff
+
+  save_png prefix: 'ttwc_'
+  #save_pdf trim: 37.5
+end
+
+Squib::Deck.new(dpi: 300, width: 820, height: 820, cards: 1, layout: 'armorcardlayout.yml')  do
+
+  ## overall card stuff
+
+  background color: 'black'
+  rect layout: 'cut'
+  rect layout: 'backOfCards'
+  svg data: GameIcons.get('swords-emblem').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'emblemBack'
+  ## output file stuff
+
+  save_png prefix: 'ttwc_BACK'
+  #save_pdf trim: 37.5
+end
