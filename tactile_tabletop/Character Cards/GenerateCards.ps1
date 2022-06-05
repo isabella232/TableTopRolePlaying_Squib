9d6d60a66007ee39e11e53b44c725ff6dc6f26d0
Jason Nelson <jason.andr.nelson@gmail.com>
2022-06-05 15:15:35 -0700

    powershell script to generate csvs of the excel files, pass it through the ruby script, and then do cleanup/renaming

---
+++ tactile_tabletop/Character Cards/GenerateCards.ps1
@@ -0,0 +1,30 @@
+﻿#remove all old output folders
+$listOfOutputFolders = Get-ChildItem | Where-Object {$_.FullName -match "_output"}
+foreach ($outputFolder in $listOfOutputFolders)
+{
+    remove-item $outputFolder -force -recurse
+}
+
+#generate new output folders
+$listOfCharacterCardDocuments = Get-ChildItem | Where-Object {$_.FullName -match "_CC.xlsx"}
+
+foreach ($characterCardDocument in $listOfCharacterCardDocuments)
+{
+    $xlsxNameOfDocument = $characterCardDocument.Name
+    $csvNameOfDocument = $xlsxNameOfDocument -replace ".xlsx",".csv"
+    .\XlsToCsv.vbs $xlsxNameOfDocument $csvNameOfDocument
+
+    ruby generate-charactercards.rb $csvNameOfDocument
+
+    $xlsxNameOfDocument -match "_Data-(?<content>.*).xlsx"
+    $docShortName = $matches['content']
+
+    $outputDirName = resolve-path _output
+    $newOutputDirName = $outputDirName -replace "_output","_output-$docShortName"
+
+    #rename folder
+    rename-item $outputDirName $newOutputDirName
+
+    #delete old csv
+    remove-item $csvNameOfDocument
+}
