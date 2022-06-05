d98f49f3f1b5d73406f9e0daf975f785541c4891
Jason Nelson <jason.andr.nelson@gmail.com>
2022-06-05 13:36:47 -0700

    moving feature cards from Google sheets to a folder in repo

---
+++ tactile_tabletop/Feature Cards/generate-features.rb
@@ -0,0 +1,49 @@
+require 'squib'
+require 'game_icons'
+
+data = Squib.csv file: 'Tactile Tabletop Data - Feature Cards.csv'
+#grabbing icons from https://game-icons.net/
+#using gem game_icons to be able to load them
+
+
+#width/height/dpi measurements provided by template from BoardGameMaker.com, see poker-size.pdf
+Squib::Deck.new(dpi: 300, width: 820, height: 820, cards: data['Name'].size, layout: 'featurecardlayout.yml')  do
+
+  ## overall card stuff
+
+  background color: 'black'
+  rect layout: 'cut'
+  rect layout: 'safe'
+  
+  ## top ability stuff
+  #rect layout: 'FeatureTitle'
+  text str: data['Name'], layout: 'FeatureTitle'
+
+  #rect layout: 'SlotTitle'
+  text str: "Slot:", layout: 'SlotTitle'
+  #rect layout: 'SlotValue'
+  text str: data['Slot'], layout: 'SlotValue'
+  #rect layout: 'EffectTitle'
+  text str: "Effect:", layout: 'EffectTitle'
+  #rect layout: 'EffectValue'
+  text str: data['Effect'], layout: 'EffectValue'
+
+  # ## output file stuff
+
+  save_png prefix: 'ttec_'
+  #save_pdf trim: 37.5
+end
+
+Squib::Deck.new(dpi: 300, width: 820, height: 820, cards: 1, layout: 'featurecardlayout.yml')  do
+
+  ## overall card stuff
+
+  background color: 'black'
+  rect layout: 'cut'
+  rect layout: 'backOfCards'
+  svg data: GameIcons.get('battle-gear').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'emblemBack'
+  ## output file stuff
+
+  save_png prefix: 'ttec_BACK'
+  #save_pdf trim: 37.5
+end
