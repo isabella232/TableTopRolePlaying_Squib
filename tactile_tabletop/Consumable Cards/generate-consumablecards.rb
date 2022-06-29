e0b68dd9bc678d080b88745d556c2732d776b5db
Jason Nelson <jason.andr.nelson@gmail.com>
2022-06-28 18:24:48 -0700

    adding a folder for consumable cards

---
+++ tactile_tabletop/Consumable Cards/generate-consumablecards.rb
@@ -0,0 +1,53 @@
+require 'squib'
+require 'game_icons'
+
+
+if ARGV[0].nil?
+  data = Squib.csv file: 'Tactile_Tabletop_Data-Consumables.csv'
+else
+  data = Squib.csv file: ARGV[0]
+end
+
+#data = Squib.csv file: 'Tactile_Tabletop_Data-Level_2_CC.csv'
+#grabbing icons from https://game-icons.net/
+#using gem game_icons to be able to load them
+
+
+#width/height/dpi measurements provided by template from BoardGameMaker.com, see poker-size.pdf
+Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: data['Consumable Name'].size, layout: 'consumablecardlayout.yml')  do
+
+  ## overall card stuff
+
+  background color: 'white'
+  rect layout: 'cut'
+  rect layout: 'safe'
+   rect layout: 'topTitle'
+   rect layout: 'topVariables'
+   rect layout: 'topRules'
+  
+
+  text str: data['Consumable Name'], layout: 'topTitle'
+  text str: data['Consumable Variables'], layout: 'topVariables'
+  text str: data['Consumable Rules'], layout: 'topRules'
+
+  ## output file stuff
+
+  save_png prefix: 'ttconsc_'
+  #save_pdf trim: 37.5
+end
+
+Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: 1, layout: 'consumablecardlayout.yml')  do
+
+  ## overall card stuff
+
+  background color: 'white'
+  rect layout: 'cut'
+  rect layout: 'backOfCards'
+  svg data: GameIcons.get('rolling-dices').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'diceBack'
+  svg data: GameIcons.get('card-random').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'cardBack'
+  svg data: GameIcons.get('two-coins').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'tokensBack'
+  ## output file stuff
+
+  save_png prefix: 'ttcc_BACK'
+  #save_pdf trim: 37.5
+end
