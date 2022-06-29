53118c66bfc58652c300fef6769e65f1a830e0b8
Jason Nelson <jason.andr.nelson@gmail.com>
2022-06-28 18:24:34 -0700

    fixing size of back image

--- tactile_tabletop/Character Cards/generate-charactercards.rb
+++ tactile_tabletop/Character Cards/generate-charactercards.rb
@@ -110,7 +110,7 @@ Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: data['Top Ability Nam
   #save_pdf trim: 37.5
 end
 
-Squib::Deck.new(dpi: 300, width: 822, height: 1122, cards: 1, layout: 'charactercardlayout.yml')  do
+Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: 1, layout: 'charactercardlayout.yml')  do
 
   ## overall card stuff
 
