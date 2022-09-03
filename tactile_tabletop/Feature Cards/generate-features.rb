1b86adc7db98fe9c26401e7fb9bf764cf5d898ba
Jason Nelson <jason.andr.nelson@gmail.com>
2022-09-02 20:16:32 -0700

    making the feature cards look like character cards, copying over the yml and rb stuff

--- tactile_tabletop/Feature Cards/generate-features.rb
+++ tactile_tabletop/Feature Cards/generate-features.rb
@@ -11,24 +11,101 @@ Squib::Deck.new(dpi: 300, width: 820, height: 820, cards: data['Name'].size, lay
 
   ## overall card stuff
 
-  background color: 'black'
+  background color: 'white'
   rect layout: 'cut'
   rect layout: 'safe'
+  rect layout: 'topAbilityColorBox'
+  rect layout: 'topAbilityColorBoxBorderCover'
+  rect layout: 'bottomAbilityColorBox'
+  rect layout: 'passivesColorBox'
+  rect layout: 'requirementsColorBox'
+  rect layout: 'requirementsColorBoxBorderCover'
   
   ## top ability stuff
-  #rect layout: 'FeatureTitle'
-  text str: data['Name'], layout: 'FeatureTitle'
+  #rect layout: 'lineRightOfBubbles'
+  rect layout: 'topTargetBubble'
+  text str: data['Top Ability Target'], layout: 'topTarget'
+  svg data: GameIcons.get('crosshair').recolor(fg: 'aaa', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'topTargetIcon'
+  
+  rect layout: 'topRangeBubble'
+  #svg file: data['Top Weapon Or Influence'].map {|t| "#{t.downcase}.svg" }, layout: 'topRange'
+  text str: data['Top Weapon Or Influence'], layout: 'topRange'
+  text str: data['Top Ability Target'], layout: 'topTarget'
+  svg data: GameIcons.get('binoculars').recolor(fg: 'aaa', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'topRangeIcon'
+  
+  rect layout: 'topDurationBubble'
+  text str: data['Top Ability Duration'], layout: 'topDuration'
+  svg data: GameIcons.get('stopwatch').recolor(fg: 'aaa', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'topDurationIcon'
 
-  #rect layout: 'SlotTitle'
-  text str: "Slot:", layout: 'SlotTitle'
-  #rect layout: 'SlotValue'
-  text str: data['Slot'], layout: 'SlotValue'
-  #rect layout: 'EffectTitle'
-  text str: "Effect:", layout: 'EffectTitle'
-  #rect layout: 'EffectValue'
-  text str: data['Effect'], layout: 'EffectValue'
+  rect layout: 'topResultBubble'
+  text str: data['Top Ability Following Card Action'], layout: 'topResult'
+  svg data: GameIcons.get('arrow-dunk').recolor(fg: 'aaa', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'topResultIcon'
 
-  # ## output file stuff
+  
+  #rect layout: 'topTitle'
+  #rect layout: 'topVariables'
+  #rect layout: 'topRules'
+  text str: data['Top Ability Name'], layout: 'topTitle'
+  #svg file: data['Top Weapon Or Influence'].map {|t| "#{t.downcase}.svg" }, layout: 'topWeaponOrInfluence'
+  text str: data['Top Ability Die Roll/Scaler'], layout: 'topVariables'
+  text str: data['Top Ability Rules'], layout: 'topRules'
+  ## bottom ability stuff
+  
+  rect layout: 'lineTopOfBottomAbility'
+  rect layout: 'bottomTargetBubble'
+  text str: data['Bottom Ability Target'], layout: 'bottomTarget'
+  
+  rect layout: 'bottomRangeBubble'
+  #svg file: data['Bottom Weapon Or Influence'].map {|t| "#{t.downcase}.svg" }, layout: 'bottomRange'
+  text str: data['Bottom Weapon Or Influence'], layout: 'bottomRange'
+  text str: data['Bottom Ability Target'], layout: 'bottomTarget'
+  svg data: GameIcons.get('binoculars').recolor(fg: 'aaa', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'bottomRangeIcon'
+  
+  rect layout: 'bottomDurationBubble'
+  text str: data['Bottom Ability Duration'], layout: 'bottomDuration'  
+
+  rect layout: 'bottomResultBubble'
+  text str: data['Bottom Ability Following Card Action'], layout: 'bottomResult'
+  svg data: GameIcons.get('arrow-dunk').recolor(fg: 'aaa', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'bottomResultIcon'
+
+  
+  #rect layout: 'bottomTitle'
+  #rect layout: 'bottomRules'
+  #rect layout: 'bottomVariables'
+  text str: data['Bottom Ability Name'], layout: 'bottomTitle'
+  #svg file: data['Bottom Weapon Or Influence'].map {|t| "#{t.downcase}.svg" }, layout: 'bottomWeaponOrInfluence'
+  text str: data['Bottom Ability Die Roll/Scaler'], layout: 'bottomVariables'
+  svg data: GameIcons.get('crosshair').recolor(fg: '777', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'bottomTargetIcon'
+  
+  text str: data['Bottom Ability Rules'], layout: 'bottomRules'
+  svg data: GameIcons.get('stopwatch').recolor(fg: '777', bg: '000', fg_opacity: 0.6, bg_opacity: 0).string, layout: 'bottomDurationIcon'
+
+  ## passives stuff
+  
+  rect layout: 'lineTopOfPassives'
+  #rect layout: 'passivesTitle'
+  #rect layout: 'passivesBody'
+  text str: "Passives", layout: 'passivesTitle'
+  text str: data['Passives'], layout: 'passivesBody'
+  
+  rect layout: 'verticalLine'
+  #rect layout: 'tierTitle'
+  #rect layout: 'tierBody'
+  text str: "Tier:", layout: 'tierTitle'
+  text str: data['Tier'], layout: 'tierBody'
+
+  ## requirements stuff
+  
+  rect layout: 'lineTopOfRequirements'
+  #rect layout: 'requirementsTitle'
+  #rect layout: 'requirementsBody'
+  text str: "Requirements", layout: 'requirementsTitle'
+  text str: data['Requirements'], layout: 'requirementsBody'
+  
+  rect layout: 'cardNumberCircle'
+  text str: data['ID'], layout: 'cardNumber'
+
+  ## output file stuff
 
   save_png prefix: 'ttec_'
   #save_pdf trim: 37.5
