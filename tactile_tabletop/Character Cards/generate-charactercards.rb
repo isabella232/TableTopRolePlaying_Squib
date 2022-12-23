1a5348c90ab904beb5e9a2502ad31037364ae203
Jason Nelson <jason.andr.nelson@gmail.com>
2022-12-22 20:37:41 -0800

    using map functions on the new stat columns to determine whether the lines around the border should be seen, and feeds into the requirement text at the bottom of the screen

--- tactile_tabletop/Character Cards/generate-charactercards.rb
+++ tactile_tabletop/Character Cards/generate-charactercards.rb
@@ -21,6 +21,26 @@ Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: data['Top Ability Nam
 
   background color: 'white'
   rect layout: 'cut'
+  
+  # #aaa is the same as the border, so it's effectively zero'd out
+  perceptionBorderColor = data['Perception Requirements'].map {|c| c > 0 ? '#00ffff' : '#aaa'}
+  vigorBorderColor = data['Vigor Requirements'].map {|c| c > 0 ? '#008000' : '#aaa'}
+  finesseBorderColor = data['Finesse Requirements'].map {|c| c > 0 ? '#ffc0cb' : '#aaa'}
+  knowledgeBorderColor = data['Knowledge Requirements'].map {|c| c > 0 ? '#0000ff' : '#aaa'}
+  strengthBorderColor = data['Strength Requirements'].map {|c| c > 0 ? '#ff0000' : '#aaa'}
+  spiritualityBorderColor = data['Spirituality Requirements'].map {|c| c > 0 ? '#800080' : '#aaa'}
+  charismaBorderColor = data['Charisma Requirements'].map {|c| c > 0 ? '#ffd700' : '#aaa'}
+  craftmanshipBorderColor = data['Craftmanship Requirements'].map {|c| c > 0 ? '#000000' : '#aaa'}
+  
+  rect x: 750, y: 675, width: 1000, height: 30, angle: 10, fill_color: perceptionBorderColor, stroke_width: 0
+  rect x: 750, y: 705, width: 1000, height: 30, angle: 10, fill_color: vigorBorderColor, stroke_width: 0
+  rect x: 750, y: 735, width: 1000, height: 30, angle: 10, fill_color: finesseBorderColor, stroke_width: 0
+  rect x: 750, y: 765, width: 1000, height: 30, angle: 10, fill_color: knowledgeBorderColor, stroke_width: 0
+  rect x: 750, y: 795, width: 1000, height: 30, angle: 10, fill_color: strengthBorderColor, stroke_width: 0
+  rect x: 750, y: 825, width: 1000, height: 30, angle: 10, fill_color: spiritualityBorderColor, stroke_width: 0
+  rect x: 750, y: 855, width: 1000, height: 30, angle: 10, fill_color: charismaBorderColor, stroke_width: 0
+  rect x: 750, y: 885, width: 1000, height: 30, angle: 10, fill_color: craftmanshipBorderColor, stroke_width: 0
+  
   #rect layout: 'safe'
   rect layout: 'topAbilityColorBox'
   rect layout: 'topAbilityColorBoxBorderCover'
@@ -108,7 +128,33 @@ Squib::Deck.new(dpi: 300, width: 750, height: 1050, cards: data['Top Ability Nam
   #rect layout: 'requirementsTitle'
   #rect layout: 'requirementsBody'
   text str: "Requirements", layout: 'requirementsTitle'
-  text str: data['Requirements'], layout: 'requirementsBody'
+  
+  
+  #create empty array of the right size
+  requirementsText = data['Perception Requirements'].map {|val| " "}
+  #create arrays of the same size, either empty or with the info for their stats specified
+  perceptionRequirementsText = data['Perception Requirements'].map {|val| val > 0 ? val.to_s + " Per " : ""}
+  vigorRequirementsText = data['Vigor Requirements'].map {|val| val > 0 ? val.to_s + " Vig " : ""}
+  finesseRequirementsText = data['Finesse Requirements'].map {|val| val > 0 ? val.to_s + " Fin " : ""}
+  knowledgeRequirementsText = data['Knowledge Requirements'].map {|val| val > 0 ? val.to_s + " Kno " : ""}
+  strengthRequirementsText = data['Strength Requirements'].map {|val| val > 0 ? val.to_s + " Str " : ""}
+  spiritualityRequirementsText = data['Spirituality Requirements'].map {|val| val > 0 ? val.to_s + " Spi " : ""}
+  charismaRequirementsText = data['Charisma Requirements'].map {|val| val > 0 ? val.to_s + " Cha " : ""}
+  craftmanshipRequirementsText = data['Craftmanship Requirements'].map {|val| val > 0 ? val.to_s + " Cra " : ""}
+  
+  #append all the strings together across the arrays
+  #zip the arrays together, so each entry becomes a sub array of all the text
+  requirementsText = requirementsText.zip(perceptionRequirementsText, vigorRequirementsText, finesseRequirementsText, knowledgeRequirementsText, strengthRequirementsText, spiritualityRequirementsText, charismaRequirementsText, craftmanshipRequirementsText)
+  #turn the sub arrays into singular strings
+  requirementsText = requirementsText.map {|subArray| subArray * ""}
+
+
+#create a third array, which combines the contents of 2 others?
+#use map to cmobine two arrays...  
+#product will combine entries together into sub arrays... need to ignore most of the combinations,  and would need to flatten those that I'd need to concatenate the strings
+#zip?
+  
+  text str: requirementsText, layout: 'requirementsBody'
   
   rect layout: 'cardNumberCircle'
   text str: data['ID'], layout: 'cardNumber'
