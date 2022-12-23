f20197fbe8846ec1fc7e798dff3508581b92ff21
Jason Nelson <jason.andr.nelson@gmail.com>
2022-12-23 12:58:41 -0800

    removing boxes around armor card stuff, and generating separate backs

--- tactile_tabletop/Armor Cards/generate-armors.rb
+++ tactile_tabletop/Armor Cards/generate-armors.rb
@@ -16,9 +16,9 @@ Squib::Deck.new(dpi: 300, width: 530, height: 530, cards: data['Armor Name'].siz
   rect layout: 'safe'
   
   ## top ability stuff
-  rect layout: 'ArmorTitle'
+  #rect layout: 'ArmorTitle'
   text str: data['Armor Name'], layout: 'ArmorTitle'
-  rect layout: 'ArmorAbility'
+  #rect layout: 'ArmorAbility'
   text str: data['Set Ability 1'], layout: 'ArmorAbility'
 
   # ## output file stuff
@@ -38,7 +38,7 @@ Squib::Deck.new(dpi: 300, width: 530, height: 530, cards: data['Armor Name'].siz
   svg data: GameIcons.get('swords-emblem').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'emblemBack'
   ## output file stuff
 
-  #save_png prefix: 'ttac_BACK'
+  save_png prefix: 'ttac_BACK'
   #save_pdf trim: 37.5
   save_sheet prefix: 'ttac-print_BACK', columns: 3, rows: 5, margin: 0, gap: 5, trim: 35
 end
