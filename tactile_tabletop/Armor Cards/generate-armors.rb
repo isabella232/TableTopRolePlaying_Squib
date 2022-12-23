b3eb270edd02ef1d1a1d9c624d89b36c09dc16e5
SirPinkleton <jason.andr.nelson@gmail.com>
2022-12-22 16:32:38 -0800

    fixing names to properly indicte armor

--- tactile_tabletop/Armor Cards/generate-armors.rb
+++ tactile_tabletop/Armor Cards/generate-armors.rb
@@ -23,9 +23,9 @@ Squib::Deck.new(dpi: 300, width: 530, height: 530, cards: data['Armor Name'].siz
 
   # ## output file stuff
 
-  save_png prefix: 'ttwc_'
+  save_png prefix: 'ttac_'
   #save_pdf trim: 37.5
-  save_sheet prefix: 'ttwc-print_', columns: 3, rows: 5, margin: 0, gap: 5, trim: 35
+  save_sheet prefix: 'ttac-print_', columns: 3, rows: 5, margin: 0, gap: 5, trim: 35
 end
 
 Squib::Deck.new(dpi: 300, width: 530, height: 530, cards: data['Armor Name'].size, layout: 'armorcardlayout.yml')  do
@@ -38,7 +38,7 @@ Squib::Deck.new(dpi: 300, width: 530, height: 530, cards: data['Armor Name'].siz
   svg data: GameIcons.get('swords-emblem').recolor(fg: 'fff', bg: '000', fg_opacity: 1, bg_opacity: 0).string, layout: 'emblemBack'
   ## output file stuff
 
-  #save_png prefix: 'ttwc_BACK'
+  #save_png prefix: 'ttac_BACK'
   #save_pdf trim: 37.5
-  save_sheet prefix: 'ttwc-print_BACK', columns: 3, rows: 5, margin: 0, gap: 5, trim: 35
+  save_sheet prefix: 'ttac-print_BACK', columns: 3, rows: 5, margin: 0, gap: 5, trim: 35
 end
